b99cea9609dc43cfb9c16b1ae41198e72f9d4b1c
Wilton Gabriel <gabrielwilton83@gmail.com>
2025-11-10 00:48:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1315277790-e019f",
+  "appId": "1:983428119431:web:9b4bbb3d3041ab0b58b122",
+  "apiKey": "AIzaSyDFTqjdDYt8vr4mCsE-D_NB7APCQDjkDaw",
+  "authDomain": "studio-1315277790-e019f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "983428119431"
+};
